4b3fe53af47d6a3ec5478143d3bb28b672d6f3b7
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-09-26 16:59:36 -0300

    Number

--- tipos/numbers.js
+++ tipos/numbers.js
@@ -1,1 +1,14 @@
 "use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villiants = 20;
+    if (avengers < villiants) {
+        console.log("Estamos en problemas");
+    }
+    else {
+        console.log("Nos salvamos");
+    }
+    avengers = Number("55A");
+    console.log(avengers);
+})();
