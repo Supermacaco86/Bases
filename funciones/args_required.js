51f95588682db5fd295acfe0ed4d4476ced7b7b2
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-09-27 17:30:27 -0300

    funciones

---
+++ funciones/args_required.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName}`;
+    };
+    const name = fullName("Toni", "Stark");
+    console.log({ name });
+})();
